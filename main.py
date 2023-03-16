d375994a1192e502f5edcffe68589da7d4ca17ef
sevthlf <110190046+sevthlf@users.noreply.github.com>
2023-03-16 20:38:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,14 @@
-import logging
 from settings import token
-from aiogram import Bot, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.dispatcher import Dispatcher
-from aiogram.dispatcher.webhook import SendMessage
-from aiogram.utils.executor import start_webhook
+import requests
 
+base_url = 'https://api.telegram.org/bot'
+base = requests.get(f'{base_url}{token}/getUpdates')
+print(base.json())
 
-# webhook settings
-WEBHOOK_HOST = 'https://c473-188-243-183-222.eu.ngrok.io'
-WEBHOOK_PATH = ''
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+message = base.json()['result'][-1]['message']['text']
+print(message)
+user_id = base.json()['result'][-1]['message']['chat']['id']
+print(user_id)
 
-# webserver settings
-WEBAPP_HOST = '127.0.0.1'  # or ip
-WEBAPP_PORT = 8000
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=token)
-dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
-
-@dp.message_handler(commands=['start'])
-async def cmd_start(message: types.Message):
-    await bot.send_message('Привет!')
-
-@dp.message_handler(commands=['help'])
-async def cmd_help(message: types.Message):
-    await bot.send_message('Ты обратился к справке бота')
-
-async def on_startup(dp):
-   await bot.set_webhook(WEBHOOK_URL)
-   # insert code here to run it after start
-
-
-async def on_shutdown(dp):
-   logging.warning('Shutting down..')
-
-   # insert code here to run it before shutdown
-
-   # Remove webhook (not acceptable in some cases)
-   await bot.delete_webhook()
-
-   # Close DB connection (if used)
-   await dp.storage.close()
-   await dp.storage.wait_closed()
-
-   logging.warning('Bye!')
-
-
-if __name__ == '__main__':
-   start_webhook(
-       dispatcher=dp,
-       webhook_path=WEBHOOK_PATH,
-       on_startup=on_startup,
-       on_shutdown=on_shutdown,
-       skip_updates=True,
-       host=WEBAPP_HOST,
-       port=WEBAPP_PORT,
-   )
+# Here i'm using Telegram API for send message to user
+requests.get(f'{base_url}{token}/sendMessage?chat_id={user_id}&text={message}')
